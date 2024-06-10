6f02c9174b0a54b614c3574ab4bbb32af7597e10
devPharos <admin@pharosit.com.br>
2024-06-10 19:10:12 -0300

    1.0.3

--- src/config/database.js
+++ src/config/database.js
@@ -1,31 +1,15 @@
 require('dotenv').config();
 module.exports = {
-  "development": {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '0241',
-    database: 'mila-plus-dev',
-    define: {
-      timestamps: false,
-      underscored: true,
-      underscoredAll: true,
-    },
-    logging: false,
+  dialect: 'postgres',
+  host: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  define: {
+    timestamps: false,
+    underscored: true,
+    underscoredAll: true,
   },
-  "production": {
-    dialect: 'postgres',
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    define: {
-      timestamps: false,
-      underscored: true,
-      underscoredAll: true,
-    },
-    logging: false,
-  }
+  logging: false,
 };
