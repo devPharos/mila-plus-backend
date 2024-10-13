7df96505ae8ea81002154804a21261b4fc538da8
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-13 00:27:52 -0300

    Refactor database configuration and remove unnecessary code

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import Youch from 'youch';
 import routes from './routes';
 
-
 import './database';
 
 class App {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import app from './app';
 
-
 app.listen(3333, () => {
     console.log('Mila Plus')
 });
