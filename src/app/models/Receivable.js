c3d3fda15fc69d3e53829125a50a3fb295ab6ff3
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-22 02:35:43 -0300

    Add Receivable model with associations and CRUD operations

---
+++ src/app/models/Receivable.js
@@ -0,0 +1,142 @@
+import Sequelize, { Model } from 'sequelize'
+
+class Receivable extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                id: {
+                    type: Sequelize.UUID,
+                    defaultValue: Sequelize.UUIDV4,
+                    primaryKey: true,
+                },
+                company_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                filial_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                issuer_id: {
+                    type: Sequelize.UUID,
+                    allowNull: true,
+                },
+                entry_date: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                due_date: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                memo: {
+                    type: Sequelize.TEXT,
+                    allowNull: true,
+                },
+                is_recurrency: {
+                    type: Sequelize.BOOLEAN,
+                    defaultValue: false,
+                },
+                contract_number: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                amount: {
+                    type: Sequelize.FLOAT,
+                    allowNull: false,
+                },
+                fee: {
+                    type: Sequelize.FLOAT,
+                    allowNull: false,
+                },
+                total: {
+                    type: Sequelize.FLOAT,
+                    allowNull: false,
+                },
+                paymentmethod_id: {
+                    type: Sequelize.UUID,
+                    allowNull: true,
+                    references: {
+                        model: 'paymentmethods',
+                        key: 'id',
+                    },
+                },
+                status: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                status_date: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                authorization_code: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                chartofaccount_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                    references: {
+                        model: 'chartofaccounts',
+                        key: 'id',
+                    },
+                },
+                paymentcriteria_id: {
+                    type: Sequelize.UUID,
+                    allowNull: true,
+                    references: {
+                        model: 'paymentcriterias',
+                        key: 'id',
+                    },
+                },
+                created_at: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                },
+                created_by: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                updated_at: {
+                    type: Sequelize.DATE,
+                    allowNull: true,
+                },
+                updated_by: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                },
+                canceled_at: {
+                    type: Sequelize.DATE,
+                    allowNull: true,
+                },
+                canceled_by: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                },
+            },
+            {
+                sequelize,
+                tableName: 'receivables', // Nome da tabela
+            }
+        )
+
+        return this
+    }
+
+    static associate(models) {
+        this.belongsTo(models.PaymentMethod, {
+            foreignKey: 'paymentmethod_id',
+            as: 'paymentMethod',
+        })
+        this.belongsTo(models.Chartofaccount, {
+            foreignKey: 'chartofaccount_id',
+            as: 'chartOfAccount',
+        })
+        this.belongsTo(models.PaymentCriteria, {
+            foreignKey: 'paymentcriteria_id',
+            as: 'paymentCriteria',
+        })
+    }
+}
+
+export default Receivable
