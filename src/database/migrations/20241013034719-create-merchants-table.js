fec3a3a54daad2b18ccf4a17deb155ce5243472c
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-22 02:36:15 -0300

    Add migration for creating merchants table

---
+++ src/database/migrations/20241013034719-create-merchants-table.js
@@ -0,0 +1,88 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('merchants', {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                primaryKey: true,
+            },
+            company_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'companies', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            filial_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'filials', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            alias: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            address: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            city: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            state: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            zip: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            country: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            ein: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            created_by: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+            },
+            updated_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            updated_by: {
+                allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+            canceled_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            canceled_by: {
+                allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('merchants')
+    },
+}
