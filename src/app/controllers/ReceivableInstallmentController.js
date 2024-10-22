4238ea3c22ceae930f30f2621e579b81597f8eca
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-22 02:35:04 -0300

    Add ReceivableInstallmentController with CRUD operations

---
+++ src/app/controllers/ReceivableInstallmentController.js
@@ -0,0 +1,209 @@
+import Sequelize from 'sequelize'
+import MailLog from '../../Mails/MailLog'
+import databaseConfig from '../../config/database'
+import ReceivableInstallment from '../models/ReceivableInstallment'
+import Receivable from '../models/Receivable'
+import PaymentMethod from '../models/PaymentMethod'
+import ChartOfAccount from '../models/Chartofaccount'
+import PaymentCriteria from '../models/PaymentCriteria'
+
+const { Op } = Sequelize
+
+class ReceivableInstallmentController {
+    async index(req, res) {
+        try {
+            const installments = await ReceivableInstallment.findAll({
+                include: [
+                    {
+                        model: Receivable,
+                        as: 'receivable',
+                        where: { canceled_at: null },
+                    },
+                    {
+                        model: PaymentMethod,
+                        as: 'paymentMethod',
+                        where: { canceled_at: null },
+                    },
+                    {
+                        model: ChartOfAccount,
+                        as: 'chartOfAccount',
+                        where: { canceled_at: null },
+                    },
+                    {
+                        model: PaymentCriteria,
+                        as: 'paymentCriteria',
+                        where: { canceled_at: null },
+                    },
+                ],
+                where: { canceled_at: null },
+                order: [['created_at', 'DESC']],
+            })
+
+            if (!installments.length) {
+                return res.status(400).json({
+                    error: 'No installments found.',
+                })
+            }
+
+            return res.json(installments)
+        } catch (err) {
+            const className = 'ReceivableInstallmentController'
+            const functionName = 'index'
+            MailLog({ className, functionName, req, err })
+            return res.status(500).json({
+                error: err,
+            })
+        }
+    }
+
+    async show(req, res) {
+        try {
+            const { id } = req.params
+            const installment = await ReceivableInstallment.findByPk(id, {
+                where: { canceled_at: null },
+                include: [
+                    {
+                        model: Receivable,
+                        as: 'receivable',
+                        where: { canceled_at: null },
+                    },
+                    {
+                        model: PaymentMethod,
+                        as: 'paymentMethod',
+                        where: { canceled_at: null },
+                    },
+                    {
+                        model: ChartOfAccount,
+                        as: 'chartOfAccount',
+                        where: { canceled_at: null },
+                    },
+                    {
+                        model: PaymentCriteria,
+                        as: 'paymentCriteria',
+                        where: { canceled_at: null },
+                    },
+                ],
+            })
+
+            if (!installment) {
+                return res.status(400).json({
+                    error: 'Installment not found.',
+                })
+            }
+
+            return res.json(installment)
+        } catch (err) {
+            const className = 'ReceivableInstallmentController'
+            const functionName = 'show'
+            MailLog({ className, functionName, req, err })
+            return res.status(500).json({
+                error: err,
+            })
+        }
+    }
+
+    async store(req, res) {
+        const connection = new Sequelize(databaseConfig)
+        const t = await connection.transaction()
+        try {
+            const newInstallment = await ReceivableInstallment.create(
+                {
+                    ...req.body,
+                    created_at: new Date(),
+                    created_by: req.userId,
+                },
+                {
+                    transaction: t,
+                }
+            )
+            await t.commit()
+
+            return res.json(newInstallment)
+        } catch (err) {
+            await t.rollback()
+            const className = 'ReceivableInstallmentController'
+            const functionName = 'store'
+            MailLog({ className, functionName, req, err })
+            return res.status(500).json({
+                error: err,
+            })
+        }
+    }
+
+    async update(req, res) {
+        const connection = new Sequelize(databaseConfig)
+        const t = await connection.transaction()
+        try {
+            const { id } = req.params
+
+            const installmentExists = await ReceivableInstallment.findByPk(id)
+
+            if (!installmentExists) {
+                return res
+                    .status(401)
+                    .json({ error: 'Installment does not exist.' })
+            }
+
+            await installmentExists.update(
+                { ...req.body, updated_by: req.userId, updated_at: new Date() },
+                {
+                    transaction: t,
+                }
+            )
+            await t.commit()
+
+            return res.status(200).json(installmentExists)
+        } catch (err) {
+            await t.rollback()
+            const className = 'ReceivableInstallmentController'
+            const functionName = 'update'
+            MailLog({ className, functionName, req, err })
+            return res.status(500).json({
+                error: err,
+            })
+        }
+    }
+
+    async delete(req, res) {
+        const connection = new Sequelize(databaseConfig)
+        const t = await connection.transaction()
+        try {
+            const { id } = req.params
+
+            const installmentExists = await ReceivableInstallment.findByPk(id)
+
+            if (!installmentExists) {
+                return res
+                    .status(401)
+                    .json({ error: 'Installment does not exist.' })
+            }
+
+            await installmentExists.update(
+                {
+                    canceled_at: new Date(),
+                    canceled_by: req.userId,
+                    updated_at: new Date(),
+                    updated_by: req.userId,
+                },
+                {
+                    transaction: t,
+                }
+            )
+            await t.commit()
+
+            return res
+                .status(200)
+                .json({ message: 'Installment deleted successfully.' })
+        } catch (err) {
+            await t.rollback()
+            const className = 'ReceivableInstallmentController'
+            const functionName = 'delete'
+            MailLog({ className, functionName, req, err })
+            return res.status(500).json({
+                error: err,
+            })
+        }
+    }
+}
+
+export default new ReceivableInstallmentController()
