77949f3be5e85ef84056204a5eb5d2074d554e3d
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-22 02:36:19 -0300

    Add migration for creating merchantsxchartofaccounts table

---
+++ src/database/migrations/20241013034947-create-merchantsxchartofaccounts-table.js
@@ -0,0 +1,64 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('merchantsxchartofaccounts', {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                primaryKey: true,
+            },
+            company_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'companies', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            filial_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'filials', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            merchant_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: { model: 'merchants', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            chartofaccount_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'chartofaccounts', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            created_by: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+            },
+            updated_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            updated_by: {
+                allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+            canceled_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            canceled_by: {
+                allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('merchantsxchartofaccounts')
+    },
+}
