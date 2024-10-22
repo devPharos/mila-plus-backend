e2fb6eac0428616d05ba8e9c5c3489db2042ed59
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-22 02:36:05 -0300

    Add migration for creating paymentmethods table

---
+++ src/database/migrations/20241013034555-create-paymentmethods-table.js
@@ -0,0 +1,62 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('paymentmethods', {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                primaryKey: true,
+            },
+            company_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'companies', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            filial_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'filials', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            description: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            bankaccount_id: {
+                type: Sequelize.UUID,
+                allowNull: true,
+                references: { model: 'bankaccounts', key: 'id' },
+                onUpdate: 'NO ACTION',
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            created_by: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+            },
+            updated_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            updated_by: {
+                allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+            canceled_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            canceled_by: {
+                allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('paymentmethods')
+    },
+}
