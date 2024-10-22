ec8fddc71d0ca10b7db1ea7b1cba206b7548e921
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-22 02:36:29 -0300

    Add migration for creating payeeinstallments table

---
+++ src/database/migrations/20241013040226-create-payeeinstallments-table.js
@@ -0,0 +1,88 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('payeeinstallments', {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                primaryKey: true,
+            },
+            payee_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: { model: 'payees', key: 'id' },
+            },
+            installment: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            amount: {
+                type: Sequelize.FLOAT,
+                allowNull: false,
+            },
+            fee: {
+                type: Sequelize.FLOAT,
+                allowNull: false,
+            },
+            total: {
+                type: Sequelize.FLOAT,
+                allowNull: false,
+            },
+            paymentmethod_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: { model: 'paymentmethods', key: 'id' },
+            },
+            status: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            status_date: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            authorization_code: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            chartofaccount_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'chartofaccounts', key: 'id' },
+            },
+            paymentcriteria_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: { model: 'paymentcriterias', key: 'id' },
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            created_by: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+            },
+            updated_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            updated_by: {
+                allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+            canceled_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            canceled_by: {
+                allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('payeeinstallments')
+    },
+}
