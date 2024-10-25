7f9927ec4d16f96f00ef8cf881482c22bfcfbc26
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-24 21:09:16 -0300

    Refactor app.js to include CORS configuration and error handling

--- src/app.js
+++ src/app.js
@@ -1,53 +1,61 @@
-import express from 'express';
-import cors from 'cors';
-import Youch from 'youch';
-import routes from './routes';
+import express from 'express'
+import cors from 'cors'
+import Youch from 'youch'
+import routes from './routes'
 
-import './database';
+import './database'
 
 class App {
-  constructor() {
-    this.server = express();
+    constructor() {
+        this.server = express()
 
-    this.middlewares();
-    this.routes();
-    this.exceptionHandler();
-  }
+        this.middlewares()
+        this.routes()
+        this.exceptionHandler()
+    }
 
-  middlewares() {
-    this.server.use(cors());
-    this.server.use(express.json());
-    this.server.use(
-      cors({
-        origin: ['http://localhost:3000', 'https://milaplus.netlify.app'],
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true, // This option is important for handling cookies and authentication headers
-      })
-    );
-  }
+    middlewares() {
+        this.server.use(cors())
+        this.server.use(express.json())
+        this.server.use(
+            cors({
+                origin: [
+                    'http://localhost:3000',
+                    'https://milaplus.netlify.app',
+                ],
+                methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+                credentials: true, // This option is important for handling cookies and authentication headers
+            })
+        )
+    }
 
-  routes() {
-    this.server.use(routes);
-  }
+    routes() {
+        this.server.use(routes)
+    }
 
-  exceptionHandler() {
-    this.server.use(async (err, req, res, next) => {
-      // Se for ambiente de desenvolvimento, detalha o erro
-      if (process.env.NODE_ENV === 'development') {
-        const errors = await new Youch(err, req).toJSON();
-        console.log('Development Error:', errors);
-        return res.status(500).json(errors);
-      }
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            // Se for ambiente de desenvolvimento, detalha o erro
+            if (process.env.NODE_ENV === 'development') {
+                const errors = await new Youch(err, req).toJSON()
+                console.log('Development Error:', errors)
+                return res.status(500).json(errors)
+            }
 
-      // Em produção, mostre uma mensagem genérica ao invés de detalhes do erro
-      console.error('Production Error:', err);
+            // Em produção, mostre uma mensagem genérica ao invés de detalhes do erro
+            console.error('Production Error:', err)
 
-      return res.status(err.status || 500).json({
-        message: 'Something went wrong. Please try again later.',
-        error: process.env.NODE_ENV === 'development' ? err.message : undefined,
-      });
-    });
-  }
+            return res.status(err.status || 500).json({
+                message: 'Something went wrong. Please try again later.',
+                error:
+                    process.env.NODE_ENV === 'development'
+                        ? err.message
+                        : undefined,
+            })
+        })
+    }
+
+
 }
 
-export default new App().server;
+export default new App().server
