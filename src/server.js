85284328d4faf039c019be488658026c25a78ae7
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-24 21:09:27 -0300

    Refactor server.js to include graceful shutdown of the server on process termination or interruption

--- src/server.js
+++ src/server.js
@@ -1,6 +1,40 @@
-import app from './app';
+import app from './app'
+import db from './database'
 
+const server = app.listen(process.env.APP_PORT || 3333, () => {
+    console.log(
+        `[ MILA-PLUS-BACKEND ] => Server running on port ${
+            process.env.APP_PORT || 3333
+        }`
+    )
+})
 
-app.listen(3333, () => {
-    console.log('MILA Plus')
-});
+// Graceful Shutdown of the server when the process is terminated or interrupted by a signal
+process.on('uncaughtException', (error, origin) => {
+    console.error(`\n${error} received. \nOrigin: ${origin}`)
+    process.exit(1)
+})
+
+process.on('unhandledRejection', (error) => {
+    console.log(`\nUnhandled Rejection SIGNAL received. \n${error}`)
+})
+
+process.on('SIGINT', gracefulShutdown('SIGINT'))
+process.on('SIGTERM', gracefulShutdown('SIGTERM'))
+
+process.on('exit', (code) => {
+    console.log('Process exit with code:', code)
+})
+
+function gracefulShutdown(event) {
+    return (code) => {
+        console.log(
+            `\n[ MILA-PLUS-BACKEND ] => ${event} signal received with code: ${code}`
+        )
+        server.close(() => {
+            db.close()
+
+            console.log('HTTP server closed')
+        })
+    }
+}
