9bc546b5aa8d5ba4d0e6e97079a7b36f1319a1ab
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-31 03:23:30 -0300

    Add Issuer model with associations

---
+++ src/app/models/Issuer.js
@@ -0,0 +1,97 @@
+import Sequelize, { Model } from 'sequelize'
+
+class Issuer extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                id: {
+                    type: Sequelize.UUID,
+                    defaultValue: Sequelize.UUIDV4,
+                    primaryKey: true,
+                },
+                company_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    references: { model: 'companies', key: 'id' },
+                },
+                filial_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    references: { model: 'filials', key: 'id' },
+                },
+                student_id: {
+                    type: Sequelize.UUID,
+                    allowNull: false,
+                    references: { model: 'students', key: 'id' },
+                },
+                merchant_id: {
+                    type: Sequelize.UUID,
+                    allowNull: false,
+                    references: { model: 'merchants', key: 'id' },
+                },
+                name: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                email: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                phone_number: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                bank_account: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                bank_routing_number: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                bank_name: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                address: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                city: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                state: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                zip: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+                country: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+            },
+            {
+                sequelize,
+                tableName: 'issuers',
+            }
+        )
+
+        return this
+    }
+
+    static associate(models) {
+        this.belongsTo(models.Company, {
+            foreignKey: 'company_id',
+            as: 'company',
+        })
+        this.belongsTo(models.Filial, { foreignKey: 'filial_id', as: 'filial' })
+        this.belongsTo(models.Student, { foreignKey: 'student_id', as: 'student' })
+        this.belongsTo(models.Merchants, { foreignKey: 'merchant_id', as: 'merchant' })
+    }
+}
+
+export default Issuer
