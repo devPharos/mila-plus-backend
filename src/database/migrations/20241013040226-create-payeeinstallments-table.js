28d5a34b526a82253f4e7d164fe719a35618cd5d
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-11-07 23:58:12 -0300

    Adiciona campo due_date à tabela PayeeInstallments

--- src/database/migrations/20241013040226-create-payeeinstallments-table.js
+++ src/database/migrations/20241013040226-create-payeeinstallments-table.js
@@ -17,6 +17,10 @@ module.exports = {
                 type: Sequelize.INTEGER,
                 allowNull: false,
             },
+            due_date: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
             amount: {
                 type: Sequelize.FLOAT,
                 allowNull: false,
