f5d146da06b99abd390bb6e0ae1950eec8714ff9
PharosIT <admin@pharosit.com.br>
2025-01-15 13:15:19 -0300

    migrations fix

--- src/database/migrations/20241227141714-adjust-is-recurrence-payee.js
+++ src/database/migrations/20241227141714-adjust-is-recurrence-payee.js
@@ -3,17 +3,12 @@
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.removeColumn('payees', 'is_recurrency')
         await queryInterface.addColumn('payees', 'is_recurrence', {
             type: Sequelize.BOOLEAN,
             defaultValue: false,
         })
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.addColumn('payees', 'is_recurrency', {
-            type: Sequelize.BOOLEAN,
-            defaultValue: false,
-        })
         await queryInterface.removeColumn('payees', 'is_recurrence')
     },
 }
