918aa4fe72928c7548736bd595ce2f6a055b643f
PharosIT <admin@pharosit.com.br>
2025-04-09 14:03:12 -0300

    mail try catch

--- src/Mails/MailLog.js
+++ src/Mails/MailLog.js
@@ -8,27 +8,31 @@ export default function MailLog({
     req = null,
     err = null,
 }) {
-    if (process.env.NODE_ENV === 'development') {
-        console.log(`🛠️ Error @ ${className} - ${functionName}`)
-        console.error('Error:', err)
-    }
+    try {
+        if (process.env.NODE_ENV === 'development') {
+            console.log(`🛠️ Error @ ${className} - ${functionName}`)
+            console.error('Error:', err)
+        }
 
-    const title = `🛠️ Error @ ${className} - ${functionName}`
-    let content = req
-        ? `<p><strong>Method:</strong> ${req.method}<br/>
+        const title = `🛠️ Error @ ${className} - ${functionName}`
+        let content = req
+            ? `<p><strong>Method:</strong> ${req.method}<br/>
         <strong>Route:</strong> ${req.route.path}<br/>
         <strong>Company:</strong> ${req.companyId}<br/>
         <strong>User:</strong> ${req.userId}</p>
         <p><strong>Query:</strong> ${JSON.stringify(req.query)}</p>
         <p><strong>Params:</strong> ${JSON.stringify(req.params)}</p>
         <p><strong>Body:</strong> ${JSON.stringify(req.body)}</p>`
-        : ``
-    content += `<p><strong>Error:</strong> ${err}</p>`
+            : ``
+        content += `<p><strong>Error:</strong> ${err}</p>`
 
-    mailer.sendMail({
-        from: `"MILA Plus" <${process.env.MAIL_FROM}>`,
-        to: process.env.MAIL_TO,
-        subject: `MILA Plus - ${title}`,
-        html: MailLayout({ title, content, filial: '' }),
-    })
+        mailer.sendMail({
+            from: `"MILA Plus" <${process.env.MAIL_FROM}>`,
+            to: process.env.MAIL_TO,
+            subject: `MILA Plus - ${title}`,
+            html: MailLayout({ title, content, filial: '' }),
+        })
+    } catch (err) {
+        console.error('Error:', err)
+    }
 }
