8ba36df7b92067c76b89872f6725a653523a2d71
PharosIT <56044578+devPharos@users.noreply.github.com>
2025-07-04 21:07:18 -0300

    Update 20250609110632-add-field-to-chart-of-accounts.cjs

--- src/database/migrations/20250609110632-add-field-to-chart-of-accounts.cjs
+++ src/database/migrations/20250609110632-add-field-to-chart-of-accounts.cjs
@@ -3,12 +3,12 @@
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.addColumn('chartofaccounts', 'profit_and_loss', {
-            type: Sequelize.BOOLEAN,
-            defaultValue: false,
-        })
+        // await queryInterface.addColumn('chartofaccounts', 'profit_and_loss', {
+        //    type: Sequelize.BOOLEAN,
+        //    defaultValue: false,
+        // })
     },
     async down(queryInterface, Sequelize) {
-        await queryInterface.removeColumn('chartofaccounts', 'profit_and_loss')
+        // await queryInterface.removeColumn('chartofaccounts', 'profit_and_loss')
     },
 }
